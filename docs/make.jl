49f2d90d06e80149c27f7b246023b872fd5355f4
Jean Michel <jean.michel@imj-prg.fr>
2025-11-27 17:37:55 +0100

    fix docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,41 +1,49 @@
 using Documenter, Chevie
 
+DocMeta.setdocmeta!(Chevie, :DocTestSetup, :(using Chevie); recursive=true)
+
 makedocs(sitename="Chevie.jl documentation",
-         modules=[Chevie],
-           format = Documenter.HTML(
-        # Use clean URLs, unless built as a "local" build
-        prettyurls = !("local" in ARGS),
-        #canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
-        collapselevel=1
+   modules=[Chevie],
+   authors="Jean Michel <jean.michel@imj-prg.fr> and contributors",
+   format = Documenter.HTML(;
+       canonical = "https://juliadocs.github.io/Chevie.jl",
+       edit_link="main",
+       assets=String[],
     ),
-pages=[
-"index.md",
-"Infrastructure"=>[
-"format.md",
-"symbols.md",
-"nf.md"],
-"Reflection groups"=>[
-"permroot.md",
-"coxgroups.md",
-"weyl.md",
-"chars.md"],
-"Hecke algebras"=>[
-"hecke.md",
-"kl.md",
-"algebras.md"],
-"garside.md",
-"Reductive groups"=>[
-"semisimple.md",
-"cosets.md",
-"sscoset.md",
-"uch.md",
-"ct.md"],
-"Eigenspaces"=>[
-"eigen.md",
-"dseries.md"],
-"Unipotent elements"=>[
-"ucl.md",
-"urad.md"],
-"gendec.md",
-"dict.md" ]
+    pages=[
+    "index.md",
+    "Infrastructure"=>[
+    "format.md",
+    "symbols.md",
+    "nf.md"],
+    "Reflection groups"=>[
+    "permroot.md",
+    "coxgroups.md",
+    "weyl.md",
+    "chars.md"],
+    "Hecke algebras"=>[
+    "hecke.md",
+    "kl.md",
+    "algebras.md"],
+    "garside.md",
+    "Reductive groups"=>[
+    "semisimple.md",
+    "cosets.md",
+    "sscoset.md",
+    "uch.md",
+    "ct.md"],
+    "Eigenspaces"=>[
+    "eigen.md",
+    "dseries.md"],
+    "Unipotent elements"=>[
+    "ucl.md",
+    "urad.md"],
+    "gendec.md",
+    "dict.md" ],
+  warnonly=:missing_docs,
+)
+
+deploydocs(;
+    repo="github.com/jmichel7/Chevie.jl",
+    devbranch="main",
 )
